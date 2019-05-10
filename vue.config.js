bc5f8ee79e94f497c14f251a5a8fa420aad227ef
wangjiahuan <wangjiah>
2019-05-10 17:00:31 +0800

    style(all the file): format file
    format file

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  //   entry: {
+  //     app: './src/main.ts'
+  //   },
+  //   rules: [
+  //     {
+  //       test: /\.ts$/,
+  //       exclude: /node_modules/,
+  //       enforce: 'pre',
+  //       loader: 'tslint-loader'
+  //     },
+  //     {
+  //       test: /\.tsx?$/,
+  //       loader: 'ts-loader',
+  //       exclude: /node_modules/,
+  //       options: {
+  //         appendTsSuffixTo: [/\.vue$/]
+  //       }
+  //     }
+  //   ],
+  //   resolve: {
+  //     extensions: ['.js', '.vue', '.json', '.ts'],
+  //     alias: {
+  //       '@': resolve('src')
+  //     }
+  //   }
+}
