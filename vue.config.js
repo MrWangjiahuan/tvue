7fc2e6267cb6d5e8658940013de7c8462019cd79
wangjiahuan <wangjiah>
2019-05-10 17:36:45 +0800

    feat(main.ts,babel.config.js,vue.config.js): import ant-design-vue
    Implementing on-demand loading of antd components

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,10 @@
 module.exports = {
-  //   entry: {
-  //     app: './src/main.ts'
-  //   },
-  //   rules: [
-  //     {
-  //       test: /\.ts$/,
-  //       exclude: /node_modules/,
-  //       enforce: 'pre',
-  //       loader: 'tslint-loader'
-  //     },
-  //     {
-  //       test: /\.tsx?$/,
-  //       loader: 'ts-loader',
-  //       exclude: /node_modules/,
-  //       options: {
-  //         appendTsSuffixTo: [/\.vue$/]
-  //       }
-  //     }
-  //   ],
-  //   resolve: {
-  //     extensions: ['.js', '.vue', '.json', '.ts'],
-  //     alias: {
-  //       '@': resolve('src')
-  //     }
-  //   }
+  css: {
+    loaderOptions: {
+      less: {
+        // 解决less .bezierEasingMixin();问题
+        javascriptEnabled: true
+      }
+    }
+  }
 }
