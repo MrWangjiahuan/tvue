25bcebd75e75142a2286dc111cbe984eae177bfb
wangjiahuan <wangjiah>
2019-06-26 15:57:41 +0800

    feat(registerServiceWorker): registerServiceWorker=>router
    registerServiceWorker=>router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 require('promise.prototype.finally').shim()
 import Vue from 'vue'
+import Component from 'vue-class-component'
 import VueI18n from 'vue-i18n'
 import enUS from '@/locale/enUS'
 import zhCN from '@/locale/zhCN'
@@ -8,6 +9,14 @@ import router from '@/router/index'
 import store from '@/store'
 import { Initializer } from '@/core/'
 import '@/core/lazy_use'
+import './registerServiceWorker'
+
+// 注册路由钩子函数
+Component.registerHooks([
+  'beforeRouteEnter',
+  'beforeRouteLeave',
+  'beforeRouteUpdate'
+])
 
 Vue.use(VueI18n)
 const i18n = new VueI18n({
